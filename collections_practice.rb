9c14aeda543f00d7d1a1146b23c1883a4c2aaac1
abstract-animation-9914 <vm930@hotmail.com>
2019-02-12 21:18:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -76,21 +76,6 @@ def organize_schools(array)
   new_hash
 end 
 
-# def organize_schools(schools)
-#   organized_schools = {}
-#   schools.each do |name, location_hash|
-#     location = location_hash[:location]
-#     if organized_schools[location]
-#       organized_schools[location] << name
-#     else
-#       organized_schools[location] = []
-#       organized_schools[location] << name
-#     end
-#   end
-#   organized_schools
-# end
-
-
 
 # def count_elements(array)
 #   array.each do |original_hash|
