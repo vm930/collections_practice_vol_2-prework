21ad83b6f0a54149835a0f45e09f7495a026225d
abstract-animation-9914 <vm930@hotmail.com>
2019-02-12 21:31:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -83,15 +83,3 @@ end
 
 #[{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}]
 
-
-# def count_elements(array)
-#   array.each do |original_hash|
-#     original_hash[:count] = 0
-#     name = original_hash[:name]
-#     array.each do |hash|
-#       if hash[:name] == name
-#         original_hash[:count] += 1
-#       end
-#     end
-#   end.uniq
-# end
