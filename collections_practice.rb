538e56bbb6dc7a1fcb3060b423bacfd16117c954
abstract-animation-9914 <vm930@hotmail.com>
2019-02-12 21:13:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,7 +56,8 @@ def remove_non_strings (array)
 
 def find_cool(array)
   new_array = []
-  array.each do |element| new_array << element if element[:temperature] == "cool"
+  array.each do |element| 
+    new_array << element if element[:temperature] == "cool"
   end 
   new_array
 end 
