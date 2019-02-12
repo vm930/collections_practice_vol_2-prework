9cf6ab97a8de1666c7b1efacb548acd4009b00e9
abstract-animation-9914 <vm930@hotmail.com>
2019-02-12 21:02:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,84 +1,121 @@
-# your code goes here
-def organize_schools(schools)
-  organized_schools = {}
-  schools.each do |name, location_hash|
-    location = location_hash[:location]
-    if organized_schools[location]
-      organized_schools[location] << name
-    else
-      organized_schools[location] = []
-      organized_schools[location] << name
-    end
-  end
-  organized_schools
-end
 
-def begins_with_r(array)
-  flag = true
-  array.each do |element|
-    flag = false if element[0] != "r"
-  end
-  flag
-end
+def begins_with_r (array)
+  array.all? do |item| item[0].include?("r")? true:false
+  end 
+end 
 
 def contain_a(array)
-  container = []
-  array.each do |element|
-    container << element if element.include?("a")
-  end
-  container
-end
-
-def remove_non_strings(array)
-  container = []
-  array.each do |element|
-    container << element if element.is_a?(String)
-  end
-  container
-end
+  array.map do |item| item.include?("a")? item: nil
+  end.compact!
+end 
 
 def first_wa(array)
-  first_wa = nil
-  array.each do |element|
-    if element.match(/wa/)
-      first_wa = element 
+  element = []
+  array.each do |ele| 
+    if ele.match(/wa/)
+      element.push(ele)
       break
     end
-  end
-  first_wa
+  end 
+  element.first
 end
 
-def find_cool(array)
-  container = []
-  array.each do |element|
-    container << element if element[:temperature] == "cool" 
+def remove_non_strings (array)
+  new_array = []
+  array.each do |ele| 
+    if ele.instance_of?(String) 
+      new_array.push(ele)
+    end
+   end 
+   new_array
   end
-  container
-end
 
 def count_elements(array)
-  array.each do |original_hash|
-    original_hash[:count] = 0
-    name = original_hash[:name]
-    array.each do |hash|
-      if hash[:name] == name
-        original_hash[:count] += 1
-      end
-    end
-  end.uniq
+  counter_hash = {}
+  counter = 0
+  comparison_hash = array[0][:name]
+  #loop thru each element in the array [hash1, hash2, hash3]
+  array.each do |element|
+  #evaluate if each hash is same as previous element
+      if element[:name] == comparison_hash  # "blake" == "blake" 
+      #if so make a count
+      counter +=1
+      counter_hash[:count] = counter
+      else # if not, create a new count 
+      counter_hash[:name] = element[:name]
+      counter_hash[:count] = 1
+  end 
 end
+    counter_hash
+  end 
 
-def merge_data(keys, values)
-  container = []
-  keys.each do |person_name|
-    name = person_name[:first_name]
-    values.each do |person_data|
-      if person_data[name]
-        merged_person = person_data[name]
-        merged_person[:first_name] = name
-        container << merged_person
-      end
+#[{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}]
+
+def merge_data(array)
+end 
+
+def find_cool(array)
+  array.each do 
+    |element|if element[:temperature] == "cool"
+    element[:temperature]
     end
-  end
-  container
-end
+  end 
+end 
+
+def organize_schools(array) 
+  array.each do 
+  |element| if element == element[:location]
+      return element[:location]
+      break
+    end 
+  end 
+end 
+# def organize_schools(schools)
+#   organized_schools = {}
+#   schools.each do |name, location_hash|
+#     location = location_hash[:location]
+#     if organized_schools[location]
+#       organized_schools[location] << name
+#     else
+#       organized_schools[location] = []
+#       organized_schools[location] << name
+#     end
+#   end
+#   organized_schools
+# end
+
+
+# def find_cool(array)
+#   container = []
+#   array.each do |element|
+#     container << element if element[:temperature] == "cool" 
+#   end
+#   container
+# end
+
+# def count_elements(array)
+#   array.each do |original_hash|
+#     original_hash[:count] = 0
+#     name = original_hash[:name]
+#     array.each do |hash|
+#       if hash[:name] == name
+#         original_hash[:count] += 1
+#       end
+#     end
+#   end.uniq
+# end
+
+# def merge_data(keys, values)
+#   container = []
+#   keys.each do |person_name|
+#     name = person_name[:first_name]
+#     values.each do |person_data|
+#       if person_data[name]
+#         merged_person = person_data[name]
+#         merged_person[:first_name] = name
+#         container << merged_person
+#       end
+#     end
+#   end
+#   container
+# end
